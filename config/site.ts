b795950f6e4bcde17800f14db57201d679fd630d
myounatan <matyounatan@gmail.com>
2023-12-04 22:06:42 -0500

    fix header title

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "younatan.io",
+  name: "younatan.ca",
   description:
     "Matthew Younatan Portfolio.",
   mainNav: [
